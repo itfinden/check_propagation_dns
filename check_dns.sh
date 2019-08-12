9225a9c4e728eba20c8d144c645cb13eb233a0c1
ITFINDEN SCRIPT UTILS <50462497+itfinden@users.noreply.github.com>
2019-08-12 18:01:23 -0400

    Update check_dns.sh

--- check_dns.sh
+++ check_dns.sh
@@ -7,17 +7,67 @@
 DOMAIN=""
 REGEX="^[a-zA-Z0-9\.-]*(.com|.net|.org|.gov|.br|.cl|.uk)$"
 
-SERVERIPS="8.8.8.8 208.67.222.222 68.94.156.1 4.2.2.1 203.23.236.66 202.83.95.227 189.38.95.95 200.221.11.100 202.27.158.40 212.158.248.5 9.9.9.9"
-SERVERNAMES="[US] Google Public DNS;[US] OpenDNS;[US] AT&T;[US] Level 3;
-             [AU] Comcel;[AU] OpenNIC;[BR] GigaDNS;[BR] Universo Online (UOL);
-             [NZ] Xtra;[UK] Bulldog Broadband;
-             [US] QuadNine"
+SERVERIPS="
+8.8.8.8 
+8.8.4.4
+208.67.222.222 
+208.67.220.220 
+68.94.156.1 
+4.2.2.1 
+203.23.236.66 
+202.83.95.227 
+189.38.95.95 
+200.221.11.100 
+202.27.158.40 
+212.158.248.5 
+9.9.9.9 
+149.112.112.112 
+1.1.1.1 
+1.0.0.1 
+185.228.168.9 
+185.228.169.9 
+64.6.64.6 
+64.6.65.6 
+198.101.242.72 
+23.253.163.53 
+176.103.130.130 
+176.103.130.131"
+
+
+
+
+
+SERVERNAMES="
+			[US] Google Public 1;
+			[US] Google Public 2;
+			[US] OpenDNS #1;
+			[US] OpenDNS #2;
+			[US] AT&T;
+			[US] Level 3;
+			[AU] Comcel;
+			[AU] OpenNIC;
+			[BR] GigaDNS;
+			[BR] Universo Online (UOL);
+			[NZ] Xtra;
+			[UK] Bulldog Broadband;
+			[US] QuadNine #1;
+			[US] QuadNine #2;
+			[US] Cloudflare #1;
+			[US] Cloudflare #2;
+			[US] CleanBrowsing #1;
+			[US] CleanBrowsing #2;
+			[US] Verisign #1;
+			[US] Verisign #2;
+			[US] Alternate DNS #1;
+			[US] Alternate DNS #2;
+			[US] AdGuard DNS #1;
+			[US] AdGuard DNS #2"
 
 USAGE="
 USAGE: "${0##*/}" DOMAIN
 
 EXAMPLE
- $ ./"${0##*/}" joselop.es
+ $ ./"${0##*/}" itfinden.com
 
 NOTE
  DOMAIN MUST be a valid domain like www.itfinden.com or 
@@ -38,8 +88,8 @@ main(){
     local name
 
     # Output's header
-    printf "%-30s %15s\n" "     SERVIDOR DNS" "IP REGISTRADA"
-    echo "     ========================= ==============="
+    printf "%-30s %50s\n" "     SERVIDOR DNS" "IP REGISTRADA"
+    echo "     ============================================================= ==============="
     
     # For each IP, gets its server name,
     # updates server name's list and
@@ -49,19 +99,10 @@ main(){
         name="$(echo $SERVERNAMES | cut -d\; -f1)"
         SERVERNAMES="$(echo $SERVERNAMES | cut -d\; -f2-)" 
         
-        printf "%-30s %15s\n" "$name" "$(answer "$ip" "$DOMAIN")"
+        printf "%-50s %31s\n" "$name ($ip)"  "$(answer "$ip" "$DOMAIN")"
     done
 
     return $?
-}
-
-answer(){
-#
-# This is the program's core system.
-# Receives DNS's IP and Domain through $1 and $2.
-# Returns the IP associated to that Domain.
-#
-    dig @"$1" "$2" | grep -a1 "ANSWER SECTION" | tail -1 | awk '{print $NF}'
 }
 
 registers(){
@@ -71,6 +112,9 @@ registers(){
     local ip
     local name
     
+    ip=0;
+    name="";
+
     # Output's header
     printf "%-30s %15s\n" "     DNS SERVER" "IP ADDRESS"
     echo "     ========================= ==============="
@@ -85,6 +129,17 @@ registers(){
     return $?
 }
 
+answer(){
+#
+# This is the program's core system.
+# Receives DNS's IP and Domain through $1 and $2.
+# Returns the IP associated to that Domain.
+#
+    dig @"$1" "$2" | grep -a1 "ANSWER SECTION" | tail -1 | awk '{print $NF}'
+}
+
+
+
 
 #
 # Main
@@ -93,7 +148,7 @@ registers(){
 if [ $# -gt "0" ]; then
     DOMAIN="$1"
 else
-    echo -n "Domain: "
+    echo -n "Ingrese Dominio: "
     read -r
     DOMAIN="$REPLY"
 fi
